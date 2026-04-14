8907e8a1393f8e7988fd386a9bd332e97f3f8952
Hubert Krzemieniewski <hubert.krzem4@gmail.com>
2026-04-14 16:46:40 +0100

    Added tile input validation

--- tiles.py
+++ tiles.py
@@ -1,3 +1,36 @@
 '''
 File defining the tile object within rummikub
+
+Tiles just need to hold a colour and a value,
+Once initiated these values cannot be modified
 '''
+
+# from dataclasses import dataclass
+
+# @dataclass(frozen=True)
+class Tile:
+    validColours = ["Red", "Blue", "Black", "Orange"]
+    validValues = [i for i in range(1, 14)]
+
+    # Only accept: values -> 1 - 13 ; colour -> Red, Black, Blue, Orange
+    def __init__(self, colour, value):
+
+        if not isinstance(colour, str):
+            raise TypeError(f"Colour must be a string")
+        colour = colour.capitalize()
+
+        if not colour in self.validColours:
+            raise ValueError(f"Colour must be one of {self.validColours}")
+        
+        if not value in self.validValues:
+            raise ValueError(f"Value must be within range {[self.validValues[0], self.validValues[-1]]}")
+        
+        self.colour = colour
+        self.value = value
+
+    def __str__(self):
+        return f"{self.colour} {self.value}"
+    
+tile = Tile("red", 5)
+
+print(tile)
