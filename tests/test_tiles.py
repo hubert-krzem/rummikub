14f360aed50cfc594086306210b6ccf3613a7442
Hubert Krzemieniewski <hubert.krzem4@gmail.com>
2026-04-15 19:10:21 +0100

    Changed tile colours to be represented by enum values instead of strings, refactored tests to work with new change.

--- tests/test_tiles.py
+++ tests/test_tiles.py
@@ -1,50 +1,46 @@
 import pytest
-from tiles import Tile
+from tiles import Tile, Colour
 
 # Valid string input
 def test_str_valid():
-    tile = Tile("Red", 5)
+    tile = Tile(Colour.RED, 5)
     assert (str(tile) == "Red 5")
 
 # Test invalid inputs
 def test_invalid_colour():
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         tile = Tile("Purple", 5)
 
 def test_invalid_value_14():
     with pytest.raises(ValueError):
-        tile = Tile("Red", 14)
+        tile = Tile(Colour.RED, 14)
 
 def test_invalid_value_0():
     with pytest.raises(ValueError):
-        tile = Tile("Red", 0)
+        tile = Tile(Colour.RED, 0)
 
 def test_invalid_colour_type():
     with pytest.raises(TypeError):
-        tile = Tile(1, 0)
+        tile = Tile(1, 5)
 
 def test_invalid_value_type():
     with pytest.raises(TypeError):
-        tile = Tile("Red", "1")
+        tile = Tile(Colour.RED, "1")
 
 
 # Valid colour input tests
 def test_init_red():
-    tile = Tile("Red", 5)
-    assert tile.colour == "Red"
-
-def test_init_black():
-    tile = Tile("Black", 5)
-    assert tile.colour == "Black"
+    tile = Tile(Colour.RED, 5)
+    assert tile.colour == Colour.RED
 
 def test_init_blue():
-    tile = Tile("Blue", 5)
-    assert tile.colour == "Blue"
+    tile = Tile(Colour.BLUE, 5)
+    assert tile.colour == Colour.BLUE
+
+def test_init_black():
+    tile = Tile(Colour.BLACK, 5)
+    assert tile.colour == Colour.BLACK
 
 def test_init_orange():
-    tile = Tile("Orange", 5)
-    assert tile.colour == "Orange"
-
-def test_str_capitalization():
-    tile = Tile("red", 5)
-    assert tile.colour == "Red"
+    tile = Tile(Colour.ORANGE, 5)
+    assert tile.colour == Colour.ORANGE
--- tiles.py
+++ tiles.py
@@ -5,20 +5,23 @@ Tiles just need to hold a colour and a value,
 Once initiated these values cannot be modified
 '''
 
-# from dataclasses import dataclass
-# @dataclass(frozen=True)
+from enum import Enum
+
+class Colour(Enum):
+    RED = "Red"
+    BLUE = "Blue"
+    BLACK = "Black"
+    ORANGE = "Orange"
+
 class Tile:
-    # Only accept: values -> 1 - 13 ; colour -> Red, Black, Blue, Orange
-    validColours = ["Red", "Blue", "Black", "Orange"]
-    validValues = [i for i in range(1, 14)]
+    # Only accept: values -> 1 - 13;
+    # Valid colours defined in the colours class
+    validValues = list(range(1,14))
 
     def __init__(self, colour, value):
 
-        if not isinstance(colour, str):
-            raise TypeError(f"Colour must be a string")
-        colour = colour.capitalize()
-        if not colour in self.validColours:
-            raise ValueError(f"Colour must be one of {self.validColours}")
+        if not isinstance(colour, Colour):
+            raise TypeError(f"Colour must be a Colour enum member from: {[colour.value for colour in Colour]}")
         
         if not isinstance(value, int):
             raise TypeError(f"Value must be an integer")
@@ -29,6 +32,4 @@ class Tile:
         self.value = value
 
     def __str__(self):
-        return f"{self.colour} {self.value}"
-    
-print(Tile.validValues)
+        return f"{self.colour.value} {self.value}"
